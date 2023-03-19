c99894a0cb8276e16644ce12387571785dab8b9d
Влада <gibinav@mail.ru>
2023-03-19 19:55:47 +0300

    add tg_bot

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -1,5 +1,3 @@
-import logging
-
 from django.conf import settings
 from django.core.management import BaseCommand
 
@@ -9,73 +7,57 @@ from bot.tg.schemas import Message
 from goals.models import Goal
 
 
-logger = logging.getLogger(__name__)
-
-
 class Command(BaseCommand):
+    help = "run bot"
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.tg_client = TgClient()
+        self.tg_client = TgClient(settings.BOT_TOKEN)
 
-    def handle_unauthorized(self, tg_user: TgUser, msg: Message):
-        self.tg_client.send_message(tg_user.chat_id, 'Hello!')
-        code = tg_user.set_verification_code()
-        self.tg_client.send_message(tg_user.chat_id, f"verification code: {code}")
+    def handle_user_without_verification(self, msg: Message, tg_user: TgUser):
+        tg_user.set_verification_code()
+        tg_user.save(update_fields=["verification_code"])
+        self.tg_client.send_message(
+            msg.chat.id, f"[verification code] {tg_user.verification_code}"
+        )
 
-    def handle_authorized(self, tg_user: TgUser, msg: Message):
-        logger.info('Authorized')
-
-    def get_tasks(self, msg: Message, tg_user: TgUser):
-        goal = Goal.objects.filter(user=tg_user.user)
-        if goal.count() > 0:
-            resp_msg = [f"#{item.id} {item.title}" for item in goal]
+    def fetch_tasks(self, msg: Message, tg_user: TgUser):
+        gls = Goal.objects.filter(user=tg_user.user)
+        if gls.count() > 0:
+            resp_msg = [f"#{item.id} {item.title}" for item in gls]
             self.tg_client.send_message(msg.chat.id, "\n".join(resp_msg))
         else:
             self.tg_client.send_message(msg.chat.id, "[goals list is empty]")
 
     def handle_verified_user(self, msg: Message, tg_user: TgUser):
         if not msg.text:
-            return f"Can't be empty"
+            return
         if "/goals" in msg.text:
-            self.get_tasks(msg, tg_user)
+            self.fetch_tasks(msg, tg_user)
         else:
             self.tg_client.send_message(msg.chat.id, "[unknown command]")
 
     def handle_message(self, msg: Message):
-        tg_user, created = TgUser.objects.get_or_create(chat_id=msg.chat.id)
+        tg_user, created = TgUser.objects.get_or_create(
+            tg_id=msg.from_.id,
+            defaults={
+                "tg_chat_id": msg.chat.id,
+                "username": msg.from_.username,
+            },
+        )
+        if created:
+            self.tg_client.send_message(msg.chat.id, "[greeting]")
 
         if tg_user.user:
-            self.handle_authorized(tg_user, msg)
+            self.handle_verified_user(msg, tg_user)
         else:
-            self.handle_unauthorized(tg_user, msg)
+            self.handle_user_without_verification(msg, tg_user)
 
     def handle(self, *args, **kwargs):
         offset = 0
 
-        logger.info('Bot start handling')
         while True:
             res = self.tg_client.get_updates(offset=offset)
             for item in res.result:
                 offset = item.update_id + 1
                 self.handle_message(item.message)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
