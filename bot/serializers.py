84a17eccb76296b657ed698293a2d2e6c04f7767
Влада <gibinav@mail.ru>
2023-03-21 21:57:48 +0300

    add tg_bot

--- bot/serializers.py
+++ bot/serializers.py
@@ -9,8 +9,8 @@ class TgUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TgUser
-        read_only_fields = ("tg_id", "username", "user_id")
-        fields = ("tg_id", "username", "verification_code", "user_id")
+        read_only_fields = ("username", "user_id")
+        fields = ("username", "verification_code", "user_id")
 
     def validate(self, attrs):
         verification_code = attrs.get("verification_code")
--- bot/tg/client.py
+++ bot/tg/client.py
@@ -1,22 +1,35 @@
+import logging
+from enum import Enum
+
 import requests
 from django.conf import settings
+from bot.tg.schemas import GetUpdatesResponse, SendMessageResponse
 
-from bot.tg.dataclasses import GetUpdatesResponse, SendMessageResponse
+
+class Command(str, Enum):
+    GET_UPDATES = 'getUpdates'
+    SEND_MESSAGE = 'sendMessage'
 
 
 class TgClient:
     def __init__(self, token: str | None = None):
         self.token = token if token else settings.BOT_TOKEN
 
-    def get_url(self, method: str):
+    def get_url(self, method: str) -> str:
         return f"https://api.telegram.org/bot{self.token}/{method}"
 
     def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
-        url = self.get_url("getUpdates")
-        resp = requests.get(url, params={"offset": offset, "timeout": timeout})
-        return GetUpdatesResponse.Schema().load(resp.json())
+        data = self._get(Command.GET_UPDATES, offset=offset, timeout=timeout)
+        return GetUpdatesResponse(**data)
 
     def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
-        url = self.get_url("sendMessage")
-        resp = requests.post(url, json={"chat_id": chat_id, "text": text})
-        return SendMessageResponse.Schema().load(resp.json())
+        data = self._get(Command.SEND_MESSAGE, chat_id=chat_id, text=text)
+        return SendMessageResponse(**data)
+
+    def _get(self, command: Command, **params) -> dict:
+        url = self.get_url(command)
+        response = requests.get(url, params=params)
+        if not response.ok:
+            print(response.json())
+            raise ValueError
+        return response.json()
