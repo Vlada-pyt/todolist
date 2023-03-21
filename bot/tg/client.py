53398ebae1bcf423a7fdcd736bab07fcfbde6707
Влада <gibinav@mail.ru>
2023-03-21 18:57:29 +0300

    add tg_bot

--- bot/tg/client.py
+++ bot/tg/client.py
@@ -1,36 +1,22 @@
-import logging
-from enum import Enum
-
 import requests
 from django.conf import settings
-from bot.tg.schemas import GetUpdatesResponse, SendMessageResponse
 
-
-class Command(str, Enum):
-    GET_UPDATES = 'getUpdates'
-    SEND_MESSAGE = 'sendMessage'
+from bot.tg.dataclasses import GetUpdatesResponse, SendMessageResponse
 
 
 class TgClient:
     def __init__(self, token: str | None = None):
         self.token = token if token else settings.BOT_TOKEN
-        self.logger = logging.getLogger(__name__)
 
-    def get_url(self, method: str) -> str:
+    def get_url(self, method: str):
         return f"https://api.telegram.org/bot{self.token}/{method}"
 
     def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
-        data = self._get(Command.GET_UPDATES, offset=offset, timeout=timeout)
-        return GetUpdatesResponse(**data)
+        url = self.get_url("getUpdates")
+        resp = requests.get(url, params={"offset": offset, "timeout": timeout})
+        return GetUpdatesResponse.Schema().load(resp.json())
 
     def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
-        data = self._get(Command.SEND_MESSAGE, chat_id=chat_id, text=text)
-        return SendMessageResponse(**data)
-
-    def _get(self, command: Command, **params) -> dict:
-        url = self.get_url(command)
-        response = requests.get(url, params=params)
-        if not response.ok:
-            print(response.json())
-            raise ValueError
-        return response.json()
+        url = self.get_url("sendMessage")
+        resp = requests.post(url, json={"chat_id": chat_id, "text": text})
+        return SendMessageResponse.Schema().load(resp.json())
