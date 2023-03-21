3f5cc8342a5a90e10b0ad408ff76f9a9b3a14fb7
Влада <gibinav@mail.ru>
2023-03-21 17:44:40 +0300

    add tg_bot

--- bot/serializers.py
+++ bot/serializers.py
@@ -5,12 +5,12 @@ from bot.models import TgUser
 
 
 class TgUserSerializer(serializers.ModelSerializer):
-    verification_code = serializers.CharField(write_only=True)
+    # verification_code = serializers.CharField(write_only=True)
 
     class Meta:
         model = TgUser
-        read_only_fields = ("tg_id", "username", "user_id")
-        fields = ("tg_id", "username", "verification_code", "user_id")
+        read_only_fields = ("tg_id", "username", "user")
+        fields = ("tg_id", "username", "verification_code", "user")
 
     def validate(self, attrs):
         verification_code = attrs.get("verification_code")
@@ -18,4 +18,4 @@ class TgUserSerializer(serializers.ModelSerializer):
         if not tg_user:
             raise ValidationError({"verification_code": "field is incorrect"})
         attrs["tg_user"] = tg_user
-        return
+        return attrs
