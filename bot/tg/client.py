252c6ce0e290f0df5e82153e18a765c7a67251f1
Влада <gibinav@mail.ru>
2023-03-26 19:23:27 +0300

    fix bot

--- bot/tg/client.py
+++ bot/tg/client.py
@@ -1,16 +1,14 @@
+import logging
 from enum import Enum
 
 import requests
-from django.conf import settings
-
+from todolist import settings
 from bot.tg.schemas import GetUpdatesResponse, SendMessageResponse
 
 
 class Command(str, Enum):
     GET_UPDATES = 'getUpdates'
     SEND_MESSAGE = 'sendMessage'
-    GET_GOALS = 'goals'
-    CREATE_GOAL = 'create'
 
 
 class TgClient:
@@ -22,16 +20,19 @@ class TgClient:
 
     def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
         data = self._get(Command.GET_UPDATES, offset=offset, timeout=timeout)
+        print(data)
         return GetUpdatesResponse(**data)
 
-    def send_message(self, chat_id: int, text: str, reply_markup=None) -> SendMessageResponse:
-        if reply_markup:
-            data = self._get(Command.SEND_MESSAGE, chat_id=chat_id, text=text, reply_markup=reply_markup)
+    def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
         data = self._get(Command.SEND_MESSAGE, chat_id=chat_id, text=text)
+        print(data)
         return SendMessageResponse(**data)
 
     def _get(self, command: Command, **params) -> dict:
         url = self.get_url(command)
         response = requests.get(url, params=params)
-
+        print(response)
+        if not response.ok:
+            print(response.json())
+            raise ValueError
         return response.json()
