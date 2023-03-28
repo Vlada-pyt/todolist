7f623cd11b62e5025a88f39dd42f78fc0411e94a
Влада <gibinav@mail.ru>
2023-03-28 19:48:16 +0300

    fix bot

--- bot/models.py
+++ bot/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from core.models import User
 
 
-
 class TgUser(models.Model):
     chat_id = models.BigIntegerField(unique=True)
     username = models.CharField(max_length=350, null=True, blank=True, default=None)
